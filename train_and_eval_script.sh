c2bdafff0d9336703fc4e4f0c57ebf3b17c1c0fc
nilskk <nils.kleine.klausing@uni-duesseldorf.de>
2020-11-03 15:04:05 +0100

    new directory

--- train_and_eval_script.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-python training.py --pipeline_config_path="./_fruit_kaggle_project/models/my_ssd_mobilenetv2_fpnlite/pipeline.config" --model_dir="./_fruit_kaggle_project/models/my_ssd_mobilenetv2_fpnlite" &
-
-python evaluation.py --pipeline_config_path="./_fruit_kaggle_project/models/my_ssd_mobilenetv2_fpnlite/pipeline.config" --model_dir="./_fruit_kaggle_project/models/my_ssd_mobilenetv2_fpnlite" --checkpoint_dir="./_fruit_kaggle_project/models/my_ssd_mobilenetv2_fpnlite" &
