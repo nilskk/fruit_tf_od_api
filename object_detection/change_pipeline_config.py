7634cb6a9e000da810fcc52057a3e0b9324fce6d
nilskk <nils.kleine.klausing@uni-duesseldorf.de>
2020-11-06 16:16:35 +0100

    added script for changing pipeline.config in python

---
+++ object_detection/change_pipeline_config.py
@@ -0,0 +1,44 @@
+import tensorflow.compat.v2 as tf
+from google.protobuf import text_format
+from object_detection.protos import pipeline_pb2
+from absl import flags
+
+flags.DEFINE_string('pipeline_config_path', None, 'Path to pipeline config file.')
+flags.DEFINE_integer('train_steps', 10000, 'Number of steps to train')
+flags.DEFINE_integer('warmup_steps', 1000, 'Number of steps for warmup')
+flags.DEFINE_integer('num_classes', 1, 'Number of classes in model')
+flags.DEFINE_integer('batch_size', 4, 'Batch size for training')
+flags.DEFINE_string('label_map_path', "./data/voc_data/pascal_label_map.pbtxt", 'Path to pascal_label_map.pbtxt')
+flags.DEFINE_string('train_tfrecords_path', "./data/tfrecords/vott_train.tfrecord", 'Path to train tfrecord file')
+flags.DEFINE_string('val_tfrecords_path', "./data/tfrecords/vott_val.tfrecord", 'Path to val tfrecord file')
+
+FLAGS = flags.FLAGS
+
+
+def change_pipeline(argv):
+    flags.mark_flag_as_required('pipeline_config_path')
+    pipeline = pipeline_pb2.TrainEvalPipelineConfig()
+    with tf.io.gfile.GFile(FLAGS.pipeline_config_path, 'r') as f:
+        proto_str = f.read()
+        text_format.Merge(proto_str, pipeline)
+
+    pipeline.model.ssd.num_classes = FLAGS.num_classes
+
+    pipeline.train_config.num_steps = FLAGS.train_steps
+    pipeline.train_config.batch_size = FLAGS.batch_size
+    pipeline.train_config.optimizer.momentum_optimizer.learning_rate.cosine_decay_learning_rate.total_steps = FLAGS.train_steps
+    pipeline.train_config.optimizer.momentum_optimizer.learning_rate.cosine_decay_learning_rate.warmup_steps = FLAGS.warmup_steps
+
+    pipeline.train_input_reader.tf_record_input_reader.input_path[0] = FLAGS.train_tfrecords_path
+    pipeline.train_input_reader.label_map_path = FLAGS.label_map_path
+
+    pipeline.eval_input_reader[0].tf_record_input_reader.input_path[0] = FLAGS.val_tfrecords_path
+    pipeline.eval_input_reader[0].label_map_path = FLAGS.label_map_path
+
+    pipeline_text = text_format.MessageToString(pipeline)
+    with tf.io.gfile.GFile(FLAGS.pipeline_config_path, 'wb') as f:
+        f.write(pipeline_text)
+
+if __name__=="__main__":
+    tf.compat.v1.app.run(change_pipeline)
+
