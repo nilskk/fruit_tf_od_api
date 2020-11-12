bef1a45534458451e2283269caadb47eef2f33c5
nilskk <nils.kleine.klausing@uni-duesseldorf.de>
2020-11-12 11:53:15 +0100

    added kill at ctrl-c

--- complete_detection.sh
+++ complete_detection.sh
@@ -4,6 +4,13 @@ vott_data_dir="./data/voc_data"
 vott_output_dir="./data/tfrecords"
 vott_name="Mango"
 
+learning_rate=0.02
+batch_size=16
+num_classes=14
+train_steps=25000
+warmup_steps=1000
+
+
 python3 object_detection/create_vott_tfrecord.py --data_dir=$vott_data_dir \
                                                 --output_dir=$vott_output_dir \
                                                 --vott_sourceconnection_name=$vott_name \
@@ -20,16 +27,31 @@ python3 object_detection/change_pipeline_config.py --pipeline_config_path="${mod
                                                   --label_map_path="${vott_data_dir}/pascal_label_map.pbtxt" \
                                                   --train_tfrecords_path="${vott_output_dir}/vott_train.tfrecord" \
                                                   --val_tfrecords_path="${vott_output_dir}/vott_val.tfrecord" \
-                                                  --num_classes=14 \
-                                                  --batch_size=16 \
-                                                  --train_steps=20000 \
-                                                  --warmup_steps=2000
+                                                  --num_classes=$num_classes \
+                                                  --batch_size=$batch_size \
+                                                  --learning_rate=$learning_rate \
+                                                  --train_steps=$train_steps \
+                                                  --warmup_steps=$warmup_steps
 
 python3 object_detection/training.py --pipeline_config_path="${model_path}/pipeline.config" \
-                                    --model_dir="${model_path}/checkpoints" &
+                                    --model_dir="${model_path}/checkpoints_lr=${learning_rate}" &
+
+training_pid=$!
 
 python3 object_detection/evaluation.py --pipeline_config_path="${model_path}/pipeline.config" \
-                                      --model_dir="${model_path}/checkpoints" \
-                                      --checkpoint_dir="${model_path}/checkpoints" &
+                                      --model_dir="${model_path}/checkpoints_lr=${learning_rate}" \
+                                      --checkpoint_dir="${model_path}/checkpoints_lr=${learning_rate}" &
+
+evaluation_pid=$!
+
+trap onexit INT
+function onexit(){
+  kill -9 $training_pid
+  kill -9 $evaluation_pid
+}
+
+wait
+
+echo "Berechnung fertig für lr=${learning_rate}"
 
 
