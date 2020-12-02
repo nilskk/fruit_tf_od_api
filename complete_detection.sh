e3321197a9f48825e7ac7492afd4bd754be1946b
nilskk <nils.kleine.klausing@uni-duesseldorf.de>
2020-12-02 18:47:05 +0100

    changed save folder structure

--- complete_detection.sh
+++ complete_detection.sh
@@ -1,33 +1,41 @@
 #!/bin/bash
 
+trap onexit INT
+function onexit(){
+  kill -9 $training_pid
+  kill -9 $evaluation_pid
+  kill -term $$
+}
+
 vott_data_dir="./data/voc_data"
 vott_output_dir="./data/tfrecords"
 vott_name="Mango"
 
-learning_rate=0.06
+learning_rate=0.01
 batch_size=16
-num_classes=14
-train_steps=25000
-cosine_steps=25000
+num_classes=5
+train_steps=10000
+cosine_steps=10000
 warmup_steps=1000
 
-model_dir="my_ssd_mobilenetv2_fpnlite"
+model_dir="mobilenetv2_fpnlite_notl"
 model_path="./models/own_models/${model_dir}"
-exported_model_path="./models/exported_models/${model_dir}"
+save_path="${model_path}/lr=${learning_rate}_bs=${batch_size}_classes=${num_classes}"
 
 
-python3 object_detection/create_vott_tfrecord.py --data_dir=$vott_data_dir \
+python3 create_vott_tfrecord.py --data_dir=$vott_data_dir \
                                                 --output_dir=$vott_output_dir \
                                                 --vott_sourceconnection_name=$vott_name \
                                                 --set=train
 
-python3 object_detection/create_vott_tfrecord.py --data_dir=$vott_data_dir \
+python3 create_vott_tfrecord.py --data_dir=$vott_data_dir \
                                                 --output_dir=$vott_output_dir \
                                                 --vott_sourceconnection_name=$vott_name \
                                                 --set=val
 
 
-python3 object_detection/change_pipeline_config.py --pipeline_config_path="${model_path}/pipeline.config" \
+python3 change_pipeline_config.py --pipeline_config_path="${model_path}/pipeline.config" \
+                                                  --model_dir="${save_path}/checkpoints" \
                                                   --label_map_path="${vott_data_dir}/pascal_label_map.pbtxt" \
                                                   --train_tfrecords_path="${vott_output_dir}/vott_train.tfrecord" \
                                                   --val_tfrecords_path="${vott_output_dir}/vott_val.tfrecord" \
@@ -38,31 +46,31 @@ python3 object_detection/change_pipeline_config.py --pipeline_config_path="${mod
                                                   --cosine_steps=$cosine_steps \
                                                   --warmup_steps=$warmup_steps
 
-python3 object_detection/training.py --pipeline_config_path="${model_path}/pipeline.config" \
-                                    --model_dir="${model_path}/checkpoints_lr=${learning_rate}" &
+python3 training.py --pipeline_config_path="${model_path}/pipeline.config" \
+                                    --model_dir="${save_path}/checkpoints" &
 
 training_pid=$!
 
-python3 object_detection/evaluation.py --pipeline_config_path="${model_path}/pipeline.config" \
-                                      --model_dir="${model_path}/checkpoints_lr=${learning_rate}" \
-                                      --checkpoint_dir="${model_path}/checkpoints_lr=${learning_rate}" &
+python3 evaluation.py --pipeline_config_path="${model_path}/pipeline.config" \
+                                      --model_dir="${save_path}/checkpoints" \
+                                      --checkpoint_dir="${save_path}/checkpoints" &
 
 evaluation_pid=$!
 
-trap onexit INT
-function onexit(){
-  kill -9 $training_pid
-  kill -9 $evaluation_pid
-}
-
 wait
 
-echo "Berechnung fertig für lr=${learning_rate}"
+echo "Berechnung fertig!"
 
 python3 object_detection/exporter_main_v2.py --pipeline_config_path="${model_path}/pipeline.config" \
-                                            --trained_checkpoint_dir="${model_path}/checkpoints_lr=${learning_rate}" \
-                                            --output_directory="${exported_model_path}_lr=${learning_rate}"
+                                            --trained_checkpoint_dir="${save_path}/checkpoints" \
+                                            --output_directory="${save_path}/export"
 
-echo "Model exportiert für lr=${learning_rate}"
+echo "Model exportiert!"
+
+python3 inference_savedmodel.py --label_map_path="${vott_data_dir}/pascal_label_map.pbtxt" \
+                                --export_dir="${save_path}/export"
+
+python3 calculate_flops.py --export_dir="${save_path}/export"
+
 
 
