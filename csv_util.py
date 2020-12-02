40459aa99e3eaeac85e1f35318ff6c5e9a9a2172
nilskk <nils.kleine.klausing@uni-duesseldorf.de>
2020-12-02 18:45:11 +0100

    added csv_util.py

---
+++ csv_util.py
@@ -0,0 +1,56 @@
+import pandas as pd
+import os
+
+
+def create_dataframe(dir):
+  if not os.path.exists(dir):
+    os.mkdir(dir)
+  path = os.path.join(dir, 'metrics.csv')
+  df = pd.DataFrame(list())
+  df.to_csv(path, index=False)
+
+def write_name(dir, name):
+  if not os.path.exists(dir):
+    os.mkdir(dir)
+  path = os.path.join(dir, 'metrics.csv')
+  df = pd.DataFrame([])
+  df.insert(loc=0, column='Name', value=[name])
+  df.to_csv(path, index=False)
+
+def write_lr(dir, lr):
+  path = os.path.join(dir, 'metrics.csv')
+  df = pd.read_csv(path)
+  df.insert(loc=0, column='Learning Rate', value=[lr])
+  df.to_csv(path, index=False)
+
+def write_bs(dir, bs):
+  path = os.path.join(dir, 'metrics.csv')
+  df = pd.read_csv(path)
+  df.insert(loc=0, column='Batch Size', value=[bs])
+  df.to_csv(path, index=False)
+
+def write_params(dir, params):
+  path = os.path.join(dir, 'metrics.csv')
+  df = pd.read_csv(path)
+  df.insert(loc=0, column='Parameter', value=[params])
+  df.to_csv(path, index=False)
+
+def write_flops(dir, flops):
+  path = os.path.join(dir, 'metrics.csv')
+  df = pd.read_csv(path)
+  df.insert(loc=0, column='Flops', value=[flops])
+  df.to_csv(path, index=False)
+
+def write_speed(dir, speed):
+  path = os.path.join(dir, 'metrics.csv')
+  df = pd.read_csv(path)
+  df.insert(loc=0, column='Inference Speed', value=[speed])
+  df.to_csv(path, index=False)
+
+def write_metrics(dir, metrics):
+  path = os.path.join(dir, 'metrics.csv')
+  df = pd.read_csv(path)
+  for k in metrics:
+    df = df.drop(columns=k, axis=1, errors='ignore')
+    df.insert(loc=0, column=k, value=[metrics[k]])
+  df.to_csv(path, index=False)
