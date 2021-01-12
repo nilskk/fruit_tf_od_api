3f0d6338fdcf5225b0c58c52ba7f01367d5ea9d8
nilskk <nils.kleine.klausing@uni-duesseldorf.de>
2021-01-12 17:14:19 +0100

    added voc_data_info.py

--- voc_data_info.py
+++ voc_data_info.py
@@ -1,20 +1,57 @@
 import xml.etree.ElementTree as ET
 from pathlib import Path
+import json
+import os
+import seaborn as sns
 
-def main():
-    return
+plot_path = Path("./plots/kaggle")
+
+classes_list = ['Apple1', 'Kiwi1', 'Mango', 'Muskmelon', 'Tomato']
+
+def get_classnumber_from_xml(root, voc_class):
+    return len(root.findall("./object[name=\'"+voc_class+"\']"))
 
 def parse_voc(xml_path):
     tree = ET.parse(xml_path)
     root = tree.getroot()
-    for child in list(root):
-        print(child.get("key"))
-        if child.get("name") == "object":
-            print(child)
+    num_objects = len(root.findall('object'))
+    class_objects_dict = {"num_objects": num_objects}
+    for voc_class in classes_list:
+        class_count = get_classnumber_from_xml(root=root, voc_class=voc_class)
+        class_objects_dict[voc_class] = class_count
 
-    return
+    return class_objects_dict
+
+def calculate_imagenum_per_numobjects(dict_list):
+    imagenum_per_numobjects = {}
+    for dict in dict_list:
+        if dict['num_objects'] in imagenum_per_numobjects:
+            imagenum_per_numobjects[dict['num_objects']] += 1
+        else:
+            imagenum_per_numobjects[dict['num_objects']] = 1
+
+    return imagenum_per_numobjects
+
+def create_numobjects_graph(dict):
+    fig1 = plt.figure()
+    fig1.tight_layout()
+    plt.bar(*zip(*sorted(dict.items())))
+    plt.savefig(os.path.join(plot_path, 'num_objects_gesamt.png'))
+    
+
 
 if __name__=="__main__":
-    voc_example = Path("./data/voc_data/Annotations/Apple%201.xml")
+    plot_path.mkdir(parents=True, exist_ok=True)
 
-    parse_voc(voc_example)
+    xml_folder = Path("./data/voc_data/Annotations")
+
+    dict_list = []
+    for file in os.listdir(xml_folder):
+        xml_file = os.path.join(xml_folder, file)
+        class_dict = parse_voc(xml_path=xml_file)
+        dict_list.append(class_dict)
+
+    imagenum_per_numobjects = calculate_imagenum_per_numobjects(dict_list=dict_list)
+    print(imagenum_per_numobjects)
+
+    create_numobjects_graph(imagenum_per_numobjects)
