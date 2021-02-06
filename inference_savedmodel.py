80c8a91120856ccf1246228d8e90cc53371a2f2c
Nils Kleine Klausing <nils.kleine.klausing@uni-duesseldorf.de>
2021-02-06 15:20:42 +0100

    reduced code in inference_savedmodel.py

--- inference_savedmodel.py
+++ inference_savedmodel.py
@@ -1,13 +1,9 @@
 import time
 import os
-import math
 import random
-from object_detection.utils import label_map_util
-from object_detection.utils import visualization_utils
 import tensorflow as tf
 from PIL import Image
 import numpy as np
-import matplotlib.pyplot as plt
 from csv_util import write_speed
 from absl import flags
 import glob
@@ -28,17 +24,9 @@ def inference(argv):
     saved_model_path = os.path.join(FLAGS.export_dir, 'saved_model')
     model = tf.saved_model.load(saved_model_path)
 
-    categories = label_map_util.create_category_index_from_labelmap(FLAGS.label_map_path, use_display_name=True)
-
     num_images = len(glob.glob(FLAGS.test_image_dir + '/**/*', recursive=True))
     print(num_images)
     time_diffs = []
-
-    rows = math.ceil(math.sqrt(num_images))
-    cols = int(math.ceil(num_images / rows))
-    gs = plt.GridSpec(rows, cols)
-    fig = plt.figure(figsize=(40, 40))
-    fig.tight_layout()
 
     for i, image_name in enumerate(random.sample(os.listdir(FLAGS.test_image_dir), k=num_images)):
         image_path = os.path.join(FLAGS.test_image_dir, image_name)
@@ -54,35 +42,7 @@ def inference(argv):
         print(time_diff)
         time_diffs.append(time_diff)
 
-        num_detections = int(detections.pop('num_detections'))
-        detections = {key: value[0, :num_detections].numpy()
-                      for key, value in detections.items()}
-        detections['num_detections'] = num_detections
-
-        # detection_classes should be ints.
-        detections['detection_classes'] = detections['detection_classes'].astype(np.int64)
-
-        image_np_with_detections = image_np.copy()
-
-        visualization_utils.visualize_boxes_and_labels_on_image_array(
-            image_np_with_detections,
-            boxes=detections['detection_boxes'],
-            classes=detections['detection_classes'],
-            scores=detections['detection_scores'],
-            category_index=categories,
-            use_normalized_coordinates=True,
-            min_score_thresh=0.5
-        )
-
-        ax = fig.add_subplot(gs[i])
-        ax.set_yticklabels([])
-        ax.set_xticklabels([])
-        ax.imshow(image_np_with_detections)
-
     head, tail = os.path.split(str(FLAGS.export_dir))
-
-    png_path = os.path.join(head, 'result.png')
-    plt.savefig(png_path, bbox_inches='tight')
 
     time_diffs_array = np.array(time_diffs)
 
