5df9175e68c112800f04f35cc4e52da47d690a70
nilskk <nils.kleine.klausing@uni-duesseldorf.de>
2021-02-24 12:32:06 +0100

    rewrite collect_summary.py

--- utils/collect_summary.py
+++ utils/collect_summary.py
@@ -4,15 +4,16 @@ from absl import flags, app
 import os
 
 
-flags.DEFINE_string('own_models_dir', None, 'Path to own_models directory')
+flags.DEFINE_string('models_path', None, 'Path to models directory')
 
 FLAGS = flags.FLAGS
 
-def collect(argv):
+
+def collect_csv(models_path):
     cols_to_order = ['Name', 'Optimizer', 'Batch Size', 'Learning Rate', 'Parameter', 'Flops', 'Inference Speed',
                      'DetectionBoxes_Precision/mAP', 'Loss/total_loss']
 
-    files = glob.glob(FLAGS.own_models_dir + '/**/metrics.csv', recursive=True)
+    files = glob.glob(models_path + '/**/metrics.csv', recursive=True)
     df_list = []
     for file in files:
         tmp_df = pd.read_csv(file)
@@ -21,10 +22,17 @@ def collect(argv):
         df_list.append(tmp_df)
 
     summary_df = pd.concat(df_list, axis=0)
-    summary_path = os.path.join(FLAGS.own_models_dir, 'summary.csv')
+    summary_path = os.path.join(models_path, 'summary.csv')
     summary_df.to_csv(summary_path, index=False)
 
+
+def main(argv):
+    flags.mark_flag_as_required('models_path')
+
+    collect_csv(models_path=FLAGS.models_path)
+
+
 if __name__ == '__main__':
-    app.run(collect)
+    app.run(main)
 
 
