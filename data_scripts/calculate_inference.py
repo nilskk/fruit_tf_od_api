8ea0e3a9632f199f29fa08f978e0f894cadaa34a
nilskk <nils.kleine.klausing@uni-duesseldorf.de>
2021-02-24 12:31:07 +0100

    rewrite inference_savedmodel.py to calculate_inference.py

--- data_scripts/calculate_inference.py
+++ data_scripts/calculate_inference.py
@@ -9,16 +9,12 @@ from utils.file_util import read_tfrecord
 from absl import flags
 import glob
 
-flags.DEFINE_string('export_dir', None, 'Path to exported model')
-flags.DEFINE_string('tfrecord', './data/tfrecords/vott_val.tfrecord',
+flags.DEFINE_string('export_path', None, 'Path to exported model')
+flags.DEFINE_string('tfrecord_path', './data/tfrecords/vott_val.tfrecord',
                     'Path to tfrecord')
-flags.DEFINE_string('label_map_path', './data/voc_data/pascal_label_map.pbtxt',
-                    'Path to label map proto')
 
 FLAGS = flags.FLAGS
 
-def load_image_as_nparray(path):
-    return np.array(Image.open(path))
 
 def inference_on_single_image(model, image):
     image_tensor = tf.convert_to_tensor(image)
@@ -31,20 +27,23 @@ def inference_on_single_image(model, image):
     time_diff = end - start
     return time_diff
 
-def inference(argv):
-    flags.mark_flag_as_required('export_dir')
-    saved_model_path = os.path.join(FLAGS.export_dir, 'saved_model')
+
+def inference(export_path,
+              tfrecord_path):
+    tf.config.set_visible_devices([], 'GPU')
+
+    saved_model_path = os.path.join(export_path, 'saved_model')
     model = tf.saved_model.load(saved_model_path)
 
     time_diffs = []
-    records = read_tfrecord(FLAGS.tfrecord)
+    records = read_tfrecord(tfrecord_path)
 
     for record in records:
         time_diff = inference_on_single_image(model=model, image=record['image'])
         print(time_diff)
         time_diffs.append(time_diff)
 
-    head, tail = os.path.split(str(FLAGS.export_dir))
+    head, tail = os.path.split(str(export_path))
 
     time_diffs_array = np.array(time_diffs)
 
@@ -55,6 +54,11 @@ def inference(argv):
     write_metrics(head, metrics)
 
 
+def main(argv):
+    flags.mark_flag_as_required('export_path')
+
+    inference(export_path=FLAGS.export_path, tfrecord_path=FLAGS.tfrecord_path)
+
+
 if __name__ == "__main__":
-    tf.config.set_visible_devices([], 'GPU')
-    tf.compat.v1.app.run(inference)
+    tf.compat.v1.app.run(main)
