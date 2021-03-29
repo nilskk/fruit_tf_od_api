65a4005f9e0f0bd817655be151bfffa7af1920b7
nilskk <nils.kleine.klausing@uni-duesseldorf.de>
2021-03-29 19:46:55 +0200

    add lxml and numpy to setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,9 @@ from setuptools import find_packages, setup
 
 setup(
     name='fruitod',
-    install_requires=['seaborn'],
+    install_requires=['seaborn',
+                      'lxml',
+                      'numpy'],
     include_package_data=True,
     packages=([p for p in find_packages() if p.startswith('fruitod')])
 )
