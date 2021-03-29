6a449aeb9fc9e97de0bd0b8490bdf6d6a471951c
nilskk <nils.kleine.klausing@uni-duesseldorf.de>
2021-03-29 19:47:30 +0200

    changed voc_util.py

--- fruitod/utils/voc_util.py
+++ fruitod/utils/voc_util.py
@@ -2,37 +2,37 @@ from lxml import etree
 import numpy as np
 from object_detection.utils import label_map_util
 
-def read_voc_for_detections(voc_file_path):
+
+def read_voc_for_detections(voc_file_path, scores=False):
     root = etree.parse(voc_file_path)
+    image_id = root.find('filename').text
     objects = root.findall('object')
-    
-    detection_boxes = np.array([])
+
+    detection_boxes = np.array([]).reshape(0, 4)
     detection_classes = np.array([])
     detection_scores = np.array([])
 
-    for idx, item in enumerate(objects):
-        score = item.find('score').text
-        score_array = np.array([score]).astype(np.float)
-        
-        id = item.find('id').text
-        id_array = np.array([id]).astype(np.int64)
+    for item in objects:
+        name = item.find('name').text
+        name_array = np.array([name])
+        detection_classes = np.concatenate([detection_classes, name_array], axis=0)
+
+        if scores:
+            score = item.find('score').text
+            score_array = np.array([score]).astype(np.float)
+            detection_scores = np.concatenate([detection_scores, score_array], axis=0)
 
         bndbox = item.find('bndbox')
         ymin = bndbox.find('ymin').text
         xmin = bndbox.find('xmin').text
         ymax = bndbox.find('ymax').text
         xmax = bndbox.find('xmax').text
-        bndbox_array = np.array([ymin, xmin, ymax, xmax]).astype(np.float)
-        
-        detection_scores = np.concatenate([detection_scores, score_array], axis=0)
-        detection_classes = np.concatenate([detection_classes, id_array], axis=0)
+        bndbox_array = np.expand_dims(np.array([ymin, xmin, ymax, xmax]).astype(np.float), axis=0)
         detection_boxes = np.concatenate([detection_boxes, bndbox_array], axis=0)
 
-    print(detection_boxes.shape)
-    detections = {'detection_boxes': detection_boxes,
-                  'detection_scores': detection_scores,
-                  'detection_classes': detection_classes}
+    if detection_boxes.ndim < 2:
+        detection_boxes = np.expand_dims(detection_boxes, axis=0)
 
-    return detections
+    return image_id, detection_boxes, detection_classes, detection_scores
         
 
