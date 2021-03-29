2e8865a5c7ce1709564befdf6f84423af52cc007
nilskk <nils.kleine.klausing@uni-duesseldorf.de>
2021-03-29 16:27:46 +0200

    removed inference and flops from predict.py

--- run_scripts/predict.py
+++ run_scripts/predict.py
@@ -4,12 +4,12 @@ from fruitod.core.predict_image import predict
 from settings import *
 
 if __name__ == '__main__':
-    inference(export_path=EXPORT_PATH,
-              tfrecord_path=VAL_TFRECORD_PATH)
+    #inference(export_path=EXPORT_PATH,
+    #          tfrecord_path=VAL_TFRECORD_PATH)
 
-    flops(export_path=EXPORT_PATH)
+    #flops(export_path=EXPORT_PATH)
 
-    collect_csv(models_path=os.path.split(MODEL_PATH)[0])
+    #collect_csv(models_path=os.path.split(MODEL_PATH)[0])
 
     predict(export_path=EXPORT_PATH,
             output_path=PREDICTION_OUTPUT_PATH,
