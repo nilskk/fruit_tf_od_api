34f14909d7b1712e32fce9349e9242dda928e17f
nilskk <nils.kleine.klausing@uni-duesseldorf.de>
2021-03-31 16:20:19 +0200

    changed layout of variables

--- fruitod/settings.py
+++ fruitod/settings.py
@@ -1,25 +1,34 @@
 import os
 from pathlib import Path
 
+# Variables to customize
 HOME = os.path.join(Path.home(), 'rewe_project')
 TFRECORDS_PATH = os.path.join(HOME, 'data/tfrecords')
-TRAIN_TFRECORD_PATH = os.path.join(TFRECORDS_PATH, 'vott_train.tfrecord')
-VAL_TFRECORD_PATH = os.path.join(TFRECORDS_PATH, 'vott_val.tfrecord')
-
 VOC_PATH = os.path.join(HOME, 'data/voc_data')
-LABELMAP_PATH = os.path.join(VOC_PATH, 'pascal_label_map.pbtxt')
 VOC_SET_NAME = 'vott'
 
 MODEL_PATH = os.path.join(HOME, 'models/own_models/efficientdet_d0_notl')
-CONFIG_PATH = os.path.join(MODEL_PATH, 'pipeline.config')
 
-MODEL_NAME = os.path.basename(MODEL_PATH)
 OPTIMIZER_NAME = 'adam'
 NUM_CLASSES = 5
 BATCH_SIZE = 8
 LEARNING_RATE = 0.001
 TRAIN_EPOCHS = 10
 FIRST_DECAY_EPOCHS = 10
+
+SCORE_THRESHOLD = 0.5
+IOU_THRESHOLD = 0.95
+VISUALIZE = True
+
+
+# Inferred Variables
+TRAIN_TFRECORD_PATH = os.path.join(TFRECORDS_PATH, 'train.tfrecord')
+VAL_TFRECORD_PATH = os.path.join(TFRECORDS_PATH, 'val.tfrecord')
+LABELMAP_PATH = os.path.join(VOC_PATH, 'pascal_label_map.pbtxt')
+
+CONFIG_PATH = os.path.join(MODEL_PATH, 'pipeline.config')
+
+MODEL_NAME = os.path.basename(MODEL_PATH)
 
 SAVE_PATH = os.path.join(MODEL_PATH,
                          'lr={}_bs={}_classes={}_{}'.format(LEARNING_RATE,
@@ -28,8 +37,5 @@ SAVE_PATH = os.path.join(MODEL_PATH,
                                                             OPTIMIZER_NAME))
 CHECKPOINT_PATH = os.path.join(SAVE_PATH, 'checkpoints')
 EXPORT_PATH = os.path.join(SAVE_PATH, 'export')
-
 PREDICTION_OUTPUT_PATH = os.path.join(SAVE_PATH, 'prediction')
-SCORE_THRESHOLD = 0.7
-IOU_THRESHOLD = 0.95
-VISUALIZE = True
+
