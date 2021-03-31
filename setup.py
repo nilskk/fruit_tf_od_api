265dddea4ea132f43586446b3697600bdecf1fa8
nilskk <nils.kleine.klausing@uni-duesseldorf.de>
2021-03-31 16:20:36 +0200

    add pandas

--- setup.py
+++ setup.py
@@ -5,7 +5,8 @@ setup(
     name='fruitod',
     install_requires=['seaborn',
                       'lxml',
-                      'numpy'],
+                      'numpy',
+                      'pandas'],
     include_package_data=True,
     packages=([p for p in find_packages() if p.startswith('fruitod')])
 )
