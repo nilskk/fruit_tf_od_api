5a1e5762b2434b3b56141ad0b47e59f60d7a4bed
nilskk <nils.kleine.klausing@uni-duesseldorf.de>
2021-05-14 12:59:44 +0200

    add flops and inference calculation

--- fruitod/predict.py
+++ fruitod/predict.py
@@ -1,13 +1,15 @@
 from fruitod.core.predict_image import predict
 from fruitod.settings import *
+from fruitod.core.calculate_performance import inference, flops
+from fruitod.core.collect_summary import collect_csv
 
 if __name__ == '__main__':
-    #inference(export_path=EXPORT_PATH,
-    #          tfrecord_path=VAL_TFRECORD_PATH)
+    inference(export_path=EXPORT_PATH,
+              tfrecord_path=VAL_TFRECORD_PATH)
 
-    #flops(export_path=EXPORT_PATH)
+    flops(export_path=EXPORT_PATH)
 
-    #collect_csv(models_path=os.path.split(MODEL_PATH)[0])
+    collect_csv(models_path=os.path.split(MODEL_PATH)[0])
 
     predict(export_path=EXPORT_PATH,
             output_path=PREDICTION_OUTPUT_PATH,
