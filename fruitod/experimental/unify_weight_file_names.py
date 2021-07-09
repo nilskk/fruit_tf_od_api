ea8f06eb3b97b05008a09b1aec7c3a9e2e8e00f2
Nils Kleine Klausing <nils.kleine.klausing@uni-duesseldorf.de>
2021-07-09 18:29:21 +0200

    bugfix

--- fruitod/experimental/unify_weight_file_names.py
+++ fruitod/experimental/unify_weight_file_names.py
@@ -2,6 +2,7 @@ import shutil
 import glob
 import os
 import urllib.parse
+from pathlib import Path
 
 
 if __name__ == '__main__':
@@ -9,7 +10,11 @@ if __name__ == '__main__':
     fruit_list = ['apfel', 'bunte_paprika', 'kiwi', 'kohlrabi', 'mango', 'paprika_rot', 'tomate']
     for fruit_name in fruit_list:
         input_path = '/data/classes/{}'.format(fruit_name)
+        write_path = os.path.join(input_path, 'weights_correct_format')
         for file_path in glob.glob(os.path.join(input_path, 'weights/*')):
             if ' ' in file_path:
-                file_path = urllib.parse.quote(file_path)
-            shutil.copy2(file_path, os.path.join(input_path, 'weights_correct_format'))
+                file_name = Path(file_path).name
+                file_name_percent_encoded = urllib.parse.quote(file_name)
+                shutil.copy2(file_path, os.path.join(write_path, file_name_percent_encoded))
+            else:
+                shutil.copy2(file_path, write_path)
