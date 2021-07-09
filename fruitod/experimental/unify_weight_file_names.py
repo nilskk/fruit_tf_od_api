d6b3fb393650db3c25588c7d593110d803fbc400
Nils Kleine Klausing <nils.kleine.klausing@uni-duesseldorf.de>
2021-07-09 18:23:14 +0200

    add script to format weight files correctly

---
+++ fruitod/experimental/unify_weight_file_names.py
@@ -0,0 +1,15 @@
+import shutil
+import glob
+import os
+import urllib
+
+
+if __name__ == '__main__':
+    voc_path_string = '/data/voc_fruit_weights/'
+    fruit_list = ['apfel', 'bunte_paprika', 'kiwi', 'kohlrabi', 'mango', 'paprika_rot', 'tomate']
+    for fruit_name in fruit_list:
+        input_path = '/data/classes/{}'.format(fruit_name)
+        for file_path in glob.glob(os.path.join(input_path, 'weights/*')):
+            if ' ' in file_path:
+                file_path = urllib.parse.quote(file_path)
+            shutil.copy2(file_path, os.path.join(input_path, 'weights_correct_format'))
