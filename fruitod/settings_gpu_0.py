e1361d90979a483f59842abfbe3f7be80f848b70
nilskk <nils.kleine.klausing@uni-duesseldorf.de>
2021-07-16 14:00:32 +0200

    add minmax scaler method

--- fruitod/settings_gpu_0.py
+++ fruitod/settings_gpu_0.py
@@ -3,14 +3,16 @@ from pathlib import Path
 
 # Variables to customize
 HOME = os.path.join(Path.home(), 'rewe_project')
-VOC_PATH = os.path.join(HOME, 'data/voc_data')
-MODEL_PATH = os.path.join(HOME, 'models/two_stage_test/efficientdet_d0_5')
+VOC_PATH = os.path.join(HOME, 'data/voc_weight_test')
+MODEL_PATH = os.path.join(HOME, 'models/models_server/prob_two_stage/efficientdet_d0_iou0.5_score0.7')
+
+SCALER_METHOD = 'robust'
 
 MODEL_TYPE = "prob_two_stage"
 NUM_CLASSES = 5
 BATCH_SIZE = 8
 LEARNING_RATE = 0.001
-TRAIN_EPOCHS = 310
+TRAIN_EPOCHS = 10
 FIRST_DECAY_EPOCHS = 10
 OPTIMIZER_NAME = 'adam'
 
@@ -18,7 +20,8 @@ SCORE_THRESHOLD = 0.90
 IOU_THRESHOLD = 0.5
 VISUALIZE = True
 
-ADD_WEIGHT_INFORMATION = False
+ADD_WEIGHT_INFORMATION = True
+WEIGHT_METHOD = 'concat'
 
 
 # Inferred Variables
@@ -32,10 +35,11 @@ CONFIG_PATH = os.path.join(MODEL_PATH, 'pipeline.config')
 MODEL_NAME = os.path.basename(MODEL_PATH)
 
 SAVE_PATH = os.path.join(MODEL_PATH,
-                         'lr={}_bs={}_classes={}_{}'.format(LEARNING_RATE,
-                                                            BATCH_SIZE,
-                                                            NUM_CLASSES,
-                                                            OPTIMIZER_NAME))
+                         'lr={}_bs={}_classes={}_{}_{}'.format(LEARNING_RATE,
+                                                                BATCH_SIZE,
+                                                                NUM_CLASSES,
+                                                                OPTIMIZER_NAME,
+                                                                SCALER_METHOD))
 CHECKPOINT_PATH = os.path.join(SAVE_PATH, 'checkpoints')
 EXPORT_PATH = os.path.join(SAVE_PATH, 'export')
 PREDICTION_OUTPUT_PATH = os.path.join(SAVE_PATH, 'prediction')
