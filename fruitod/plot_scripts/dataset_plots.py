bc557366903761de78329731b666eb3a7b42db99
nilskk <nils.kleine.klausing@uni-duesseldorf.de>
2021-08-30 22:48:19 +0200

    change plot scripts

--- fruitod/plot_scripts/dataset_plots.py
+++ fruitod/plot_scripts/dataset_plots.py
@@ -119,7 +119,7 @@ if __name__ == '__main__':
 
     # Anzahl Größe der Boxen
     plt.figure()
-    ax = sns.scatterplot(data=object_dataframe, x='box_width', y='box_height', hue='class', style='size')
+    ax = sns.scatterplot(data=object_dataframe, x='width', y='height', hue='class', style='size')
     plt.savefig(os.path.join(output_directory, 'box_size.png'), bbox_inches='tight')
 
     # Seitenverhältnisse der Boxen
