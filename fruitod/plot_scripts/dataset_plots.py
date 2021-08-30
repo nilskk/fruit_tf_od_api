45a21cd59e5ed0e81ab68b8eb752717dfdb9b2e0
nilskk <nils.kleine.klausing@uni-duesseldorf.de>
2021-08-30 23:01:23 +0200

    change plot scripts

--- fruitod/plot_scripts/dataset_plots.py
+++ fruitod/plot_scripts/dataset_plots.py
@@ -106,35 +106,35 @@ if __name__ == '__main__':
 
     # Anzahl Objekte pro Klasse Train/Test
     plt.figure()
-    plt.legend(bbox_to_anchor=(1.01, 1), borderaxespad=0)
     sns.catplot(data=file_dataframe, x='class', y='objects', hue='set', split=True,
                 jitter=0.4, size=8, linewidth=1, alpha=0.5)
+    plt.legend(bbox_to_anchor=(1.01, 1), borderaxespad=0)
     plt.savefig(os.path.join(output_directory, 'objects_per_class.png'), bbox_inches='tight')
 
     # Anzahl Bilder pro Klasse Train/Test
     plt.figure()
-    plt.legend(bbox_to_anchor=(1.01, 1), borderaxespad=0)
     ax = sns.countplot(data=file_dataframe, x='class', hue='set')
     for p in ax.patches:
         ax.annotate(str(p.get_height()), (p.get_x() * 1.005, p.get_height() * 1.005))
+    plt.legend(bbox_to_anchor=(1.01, 1), borderaxespad=0)
     plt.savefig(os.path.join(output_directory, 'images_per_class.png'), bbox_inches='tight')
 
     # Anzahl Größe der Boxen
     plt.figure()
+    sns.scatterplot(data=object_dataframe, x='width', y='height', hue='class', style='size', alpha=0.8)
     plt.legend(bbox_to_anchor=(1.01, 1), borderaxespad=0)
-    ax = sns.scatterplot(data=object_dataframe, x='width', y='height', hue='class', style='size', alpha=0.8)
     plt.savefig(os.path.join(output_directory, 'box_size.png'), bbox_inches='tight')
 
     # Seitenverhältnisse der Boxen
     plt.figure()
-    plt.legend(bbox_to_anchor=(1.01, 1), borderaxespad=0)
     sns.catplot(data=object_dataframe, x='class', y='ratio', hue='set', split=True,
                 jitter=0.4, size=8, linewidth=1, alpha=0.5)
+    plt.legend(bbox_to_anchor=(1.01, 1), borderaxespad=0)
     plt.savefig(os.path.join(output_directory, 'box_ratio.png'), bbox_inches='tight')
 
     # Gewicht in Abhängigkeit von Anzahl der Objekte
     plt.figure()
-    plt.legend(bbox_to_anchor=(1.01, 1), borderaxespad=0)
     sns.scatterplot(data=file_dataframe, x='weight', y='objects', hue='class')
+    plt.legend(bbox_to_anchor=(1.01, 1), borderaxespad=0)
     plt.savefig(os.path.join(output_directory, 'weight_number_of_objects.png'), bbox_inches='tight')
 
