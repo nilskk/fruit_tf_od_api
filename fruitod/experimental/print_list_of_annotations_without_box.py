3e4fd98c1b08e55cfba4f55a578542a2dace4ba2
nilskk <nils.kleine.klausing@uni-duesseldorf.de>
2021-08-31 15:13:21 +0200

    add script to view annotations without boxes

---
+++ fruitod/experimental/print_list_of_annotations_without_box.py
@@ -0,0 +1,28 @@
+import glob
+
+from lxml import etree as ET
+from pathlib import Path
+import json
+import os
+import numpy as np
+import seaborn as sns
+import pandas as pd
+from lxml import etree
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+
+from object_detection.utils import dataset_util
+
+if __name__ == '__main__':
+    a_list = []
+    for annotation_file in glob.glob('/data/classes/*/annotations/*.xml'):
+        tree = etree.parse(annotation_file)
+        count = tree.xpath('count(//object)')
+        if count == 0:
+            a_list.append(annotation_file)
+
+    with open(os.path.join('/data/classes', 'annotations_without_box.txt'), 'w') as f:
+        for element in a_list:
+            f.write(str(element) + '\n')
+
+
