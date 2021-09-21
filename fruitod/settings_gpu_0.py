2c1670d4325629913d4f46a20394ff5b1658d32e
nilskk <nils.kleine.klausing@uni-duesseldorf.de>
2021-09-22 00:40:13 +0200

    update setting files

--- fruitod/settings_gpu_0.py
+++ fruitod/settings_gpu_0.py
@@ -2,46 +2,46 @@ import os
 from pathlib import Path
 
 # Variables to customize
-HOME = os.path.join(Path.home(), 'rewe_project')
-VOC_PATH = os.path.join(HOME, 'data/voc_weight_test')
-MODEL_PATH = os.path.join(HOME, 'models/models_server/ssd/efficientdet_d0_test')
+VOC_PATH = os.path.join(Path.home(), 'rewe_project/data/voc_weight_test')
+PIPELINE_PATH = os.path.join(Path.home(), 'rewe_project/models/models_server/prob_two_stage/efficientdet_d0_iou0.5_score0.7')
+# Just an example. Choose best model directory name on own preferences to ensure uniqueness
+MODEL_DIR = 'model_lr=0.001_input-multiply'
 
-SCALER_METHOD = 'minmax'
-ADD_WEIGHT_INFORMATION = False
-WEIGHT_METHOD = 'input-multiply'
-ADD_WEIGHT_AS_OUTPUT = False
-ADD_WEIGHT_AS_OUTPUT_V2 = False
+# !!!!! IMPORTANT !!!!!: set right MODEL_TYPE depending on pipeline.config('ssd' or 'prob_two_stage').
+# Otherwise it could falsely override pipeline.config
+MODEL_TYPE = "prob_two_stage"
 
-MODEL_TYPE = "ssd"
+ADD_WEIGHT_AS_INPUT = False     # Set to True if you want to add the whole weight as input
+
+# One of ['input-multiply', 'fpn-multiply', 'roi-multiply', 'concat']
+# For 'ssd' only 'input-multiply' and 'fpn-multiply' is supported
+INPUT_METHOD = 'input-multiply'
+
+SCALER_METHOD = 'minmax'        # Name of weight scaling method
+
+ADD_WEIGHT_AS_OUTPUT_GPO = False        # Set to True if you want to add weight per object as additional output
+ADD_WEIGHT_AS_OUTPUT_GESAMT = False     # (Only works for 'ssd') Set to True if you want to add whole weight as output
+
 NUM_CLASSES = 5
 BATCH_SIZE = 8
-LEARNING_RATE = 0.01
-TRAIN_EPOCHS = 10
+LEARNING_RATE = 0.001
+TRAIN_EPOCHS = 30
 FIRST_DECAY_EPOCHS = 10
 OPTIMIZER_NAME = 'adam'
 SCHEDULER_NAME = 'decay'
-
-SCORE_THRESHOLD = 0.90
-IOU_THRESHOLD = 0.5
-VISUALIZE = True
-
 
 
 # Inferred Variables
 TFRECORDS_PATH = VOC_PATH
 TRAIN_TFRECORD_PATH = os.path.join(TFRECORDS_PATH, 'train_gpu_0.tfrecord')
-VAL_TFRECORD_PATH = os.path.join(TFRECORDS_PATH, 'val_gpu_0.tfrecord')
-LABELMAP_PATH = os.path.join(VOC_PATH, 'pascal_label_map.pbtxt')
+TEST_TFRECORD_PATH = os.path.join(TFRECORDS_PATH, 'test_gpu_0.tfrecord')
+LABELMAP_PATH = os.path.join(VOC_PATH, 'label_map.pbtxt')
 
-CONFIG_PATH = os.path.join(MODEL_PATH, 'pipeline.config')
+CONFIG_PATH = os.path.join(PIPELINE_PATH, 'pipeline.config')
 
-MODEL_NAME = os.path.basename(MODEL_PATH)
+MODEL_NAME = os.path.basename(PIPELINE_PATH)
 
-SAVE_PATH = os.path.join(MODEL_PATH,
-                         'lr={}_bs={}_classes={}_{}'.format(LEARNING_RATE,
-                                                                BATCH_SIZE,
-                                                                NUM_CLASSES, OPTIMIZER_NAME))
-CHECKPOINT_PATH = os.path.join(SAVE_PATH, 'checkpoints')
-EXPORT_PATH = os.path.join(SAVE_PATH, 'export')
-PREDICTION_OUTPUT_PATH = os.path.join(SAVE_PATH, 'prediction')
-
+MODEL_PATH = os.path.join(PIPELINE_PATH, MODEL_DIR)
+CHECKPOINT_PATH = os.path.join(MODEL_PATH, 'checkpoints')
+EXPORT_PATH = os.path.join(MODEL_PATH, 'export')
+PREDICTION_OUTPUT_PATH = os.path.join(MODEL_PATH, 'prediction')
