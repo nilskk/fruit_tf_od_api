c52df448e4f1026bb78b34d7f5899b5f5143f3da
nilskk <nils.kleine.klausing@uni-duesseldorf.de>
2021-09-22 02:32:53 +0200

    update settings

--- fruitod/settings_gpu_0.py
+++ fruitod/settings_gpu_0.py
@@ -1,15 +1,22 @@
 import os
 from pathlib import Path
+##########################
+# Variables to customize #
+##########################
 
-# Variables to customize
-VOC_PATH = os.path.join(Path.home(), 'rewe_project/data/voc_weight_test')
-PIPELINE_PATH = os.path.join(Path.home(), 'rewe_project/models/models_server/prob_two_stage/efficientdet_d0_iou0.5_score0.7')
+# Path to VOC dataset directory
+VOC_PATH = os.path.join('/data', '<VOC_DIR>')
+
+# Path to directory with pipeline.config
+PIPELINE_PATH = os.path.join('/data', 'models/<PIPELINE_DIR>')
+
 # Just an example. Choose best model directory name on own preferences to ensure uniqueness
 MODEL_DIR = 'model_lr=0.001_input-multiply'
+MODEL_NAME = '<Modelname>'  # Change to unique name for identifying metrics later
 
-# !!!!! IMPORTANT !!!!!: set right MODEL_TYPE depending on pipeline.config('ssd' or 'prob_two_stage').
+# !!!!! IMPORTANT !!!!!: set right MODEL_TYPE depending on pipeline.config; ('ssd' or 'prob_two_stage').
 # Otherwise it could falsely override pipeline.config
-MODEL_TYPE = "prob_two_stage"
+MODEL_TYPE = 'prob_two_stage'
 
 ADD_WEIGHT_AS_INPUT = False     # Set to True if you want to add the whole weight as input
 
@@ -22,11 +29,11 @@ SCALER_METHOD = 'minmax'        # Name of weight scaling method
 ADD_WEIGHT_AS_OUTPUT_GPO = False        # Set to True if you want to add weight per object as additional output
 ADD_WEIGHT_AS_OUTPUT_GESAMT = False     # (Only works for 'ssd') Set to True if you want to add whole weight as output
 
-NUM_CLASSES = 5
-BATCH_SIZE = 8
-LEARNING_RATE = 0.001
-TRAIN_EPOCHS = 30
-FIRST_DECAY_EPOCHS = 10
+NUM_CLASSES = 5             # Number of classes (without background class!)
+BATCH_SIZE = 8              # Batch size for training
+LEARNING_RATE = 0.001       # Base Learning Rate
+TRAIN_EPOCHS = 30           # Number of Total Epochs for Training
+FIRST_DECAY_EPOCHS = 10     # Number of Warmup Epochs for CosineDecay or CosineDecayWithRestarts
 OPTIMIZER_NAME = 'adam'
 SCHEDULER_NAME = 'decay'
 
@@ -38,8 +45,6 @@ TEST_TFRECORD_PATH = os.path.join(TFRECORDS_PATH, 'test_gpu_0.tfrecord')
 LABELMAP_PATH = os.path.join(VOC_PATH, 'label_map.pbtxt')
 
 CONFIG_PATH = os.path.join(PIPELINE_PATH, 'pipeline.config')
-
-MODEL_NAME = os.path.basename(PIPELINE_PATH)
 
 MODEL_PATH = os.path.join(PIPELINE_PATH, MODEL_DIR)
 CHECKPOINT_PATH = os.path.join(MODEL_PATH, 'checkpoints')
